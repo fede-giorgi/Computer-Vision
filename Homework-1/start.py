a52b97cece33aca1c03d10cd9781f626b5759964
Federico Giorgi <federico.giorgi@studbocconi.it>
2026-02-11 22:38:59 -0500

    new structure

---
+++ Homework-1/start.py
@@ -0,0 +1,28 @@
+import cv2
+import os
+import time
+
+def get_box(img):
+    # Your code start here #
+    # Have fun!            #
+    return 0, 0, 0, 0
+
+if __name__ == "__main__":
+
+    start_time = time.time()
+
+    dir_path = './images/'
+    for i in range(1, 25):
+        img_name = f'stop{i}.png'
+        img_path = os.path.join(dir_path, img_name)
+        img = cv2.imread(img_path)
+        # Get the coordinators of the box
+        xmin, ymin, xmax, ymax = get_box(img)
+        cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
+        output_path = f'./results/{img_name}'
+        cv2.imwrite(output_path, img)
+
+    end_time = time.time()
+    #It usually takes about 10s
+    print(f"Running time: {end_time - start_time} seconds")
+
